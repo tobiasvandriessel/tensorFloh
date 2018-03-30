5d7051f0b46e5530a712206b865a034f98d332eb
Tobias van Driessel <tobiasvandriessel@startmail.com>
2018-03-30 18:15:56 +0200

    mucho kaka

--- Tensorflow_Base/matmul.cpp
+++ Tensorflow_Base/matmul.cpp
@@ -1,3 +1,7 @@
+#ifdef _MSC_VER
+#pragma warning(disable:4996)
+#endif
+
 #include <vector>
 #include <eigen/Dense>
 
@@ -7,6 +11,20 @@
 #include <ctime>
 #include <cstdio>
 
+#include <dirent.h>
+
+
+//#include <opencv2/calib3d/calib3d.hpp>
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/core/core_c.h>
+//#include <opencv2/core/mat.hpp>
+//#include <opencv2/core/operations.hpp>
+//#include <opencv2/core/types_c.h>
+//#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/highgui/highgui_c.h>
+//#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv2/imgproc/types_c.h>
+
 #include <opencv2/core.hpp>
 #include <opencv2/core/utility.hpp>
 #include <opencv2/imgproc.hpp>
@@ -14,14 +32,15 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
+//#include <opencv2/highgui/highgui_c.h>
+//#include <opencv2/imgproc/types_c.h>
+
 
 #include "matmul.h"
 #include "tensorflow/core/public/session.h"
 #include "tensorflow/cc/ops/standard_ops.h"
 
-#ifdef _MSC_VER
-#pragma warning(disable:4996)
-#endif
+
 
 using namespace cv;
 using namespace std;
@@ -47,8 +66,161 @@ GraphDef CreateGraphDef()
     return def;
 }
 
+int handleOfflineStuff() {
+	const string m_data_path = "../data/UCF-101/folds/";
+
+	DIR *dir, *dir1;
+	struct dirent *ent, *ent1;
+	if ((dir = opendir(m_data_path.c_str() )) != NULL) {
+		cout << "Entered dir: " << m_data_path << endl;
+		/* print all the files and directories within directory */
+		while ((ent = readdir(dir)) != NULL) {
+			if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
+				continue;
+
+			if ((dir1 = opendir((m_data_path + ent->d_name).c_str())) != NULL) {
+				
+				cout << "Inside loop Entered dir: " << m_data_path + ent->d_name << endl;
+
+				while ((ent1 = readdir(dir1)) != NULL) {
+					if (strcmp(ent1->d_name, ".") == 0 || strcmp(ent1->d_name, "..") == 0)
+						continue;
+
+					
+
+
+
+					extractFeaturesFromVideo(m_data_path + ent->d_name + "/" + ent1->d_name);
+					//return 0;
+
+					//printf("%s\n", ent1->d_name);
+					//cout << m_data_path + ent->d_name + "/" + ent1->d_name << endl;
+
+				}
+			}
+
+
+			printf("%s\n", ent->d_name);
+		}
+		closedir(dir);
+	}
+	else {
+		/* could not open directory */
+		perror("");
+		return EXIT_FAILURE;
+	}
+
+
+	
+}
+
+int extractFeaturesFromVideo(string path) {
+	//string videoFileLocation = "1/v_BrushingTeeth_g01_c03.avi";
+
+	// Open the video for this camera
+	auto m_video = VideoCapture(path);
+	assert(m_video.isOpened());
+
+	cout << "read video" << endl;
+
+	//// Assess the image size
+	//m_plane_size.width = (int)m_video.get(CV_CAP_PROP_FRAME_WIDTH);
+	//m_plane_size.height = (int)m_video.get(CV_CAP_PROP_FRAME_HEIGHT);
+	//assert(m_plane_size.area() > 0);
+
+	// Get the amount of video frames
+	m_video.set(CV_CAP_PROP_POS_AVI_RATIO, 1);  // Go to the end of the video; 1 = 100%
+	long m_frame_amount = (long)m_video.get(CV_CAP_PROP_POS_FRAMES);
+	cout << "Frames: " << m_frame_amount;
+	assert(m_frame_amount > 1);
+
+	m_video.set(CV_CAP_PROP_POS_AVI_RATIO, 0);  // Go back to the start
+
+	m_video.release(); //Re-open the file because _video.set(CV_CAP_PROP_POS_AVI_RATIO, 1) may screw it up
+	m_video = cv::VideoCapture(path);
+
+	int frameNumber = (int)(m_frame_amount / 2);
+
+	//m_video.set(CV_CAP_PROP_POS_FRAMES, frameNumber);  // Go back to the start
+
+	Mat m_frame;
+	m_video >> m_frame;
+	assert(!m_frame.empty());
+
+	//imshow()
+
+	cout << "path: " << path << endl;
+	cout << "Framenumber: " << frameNumber << endl;
+
+	//cvShowImage("windows", &m_frame);
+	/*imshow("windows", m_frame);
+	imshow("windows", m_frame);*/
+
+	Mat gray;
+
+	for (int i = 0; i < m_frame_amount - 1; i++) {
+		cout << i << endl;
+		m_video >> m_frame;
+		assert(!m_frame.empty());
+		cvtColor(m_frame, gray, CV_BGR2GRAY);
+		imshow("windows", gray);
+		//imshow("windows", m_frame);
+
+		//cout << m_frame << endl;
+		//cin >> path;
+		
+	}
+	//m_video >> m_frame;
+	//imshow("windows", m_frame);
+
+
+
+	/*for (int i = 0; i < 1000; i++) {
+		cout << "Hmm haha " << i << endl;
+	}*/
+
+	//cin >> path;
+	return 0;
+}
+
 int main()
 {
+	VideoCapture m_video = VideoCapture(0);
+
+	Mat m_frame;
+
+	for (int i = 0; i < 1000; i++) {
+		m_video.read(m_frame);// >> m_frame;
+		assert(!m_frame.empty());
+
+		imshow("nothing", m_frame);
+		for (int j = 0; j < 5; j++)
+			cout << "haha" + j;
+	}
+
+	string a;
+
+	cin >> a;
+
+
+
+
+	string answer;
+	//Mat a;
+	cout << "Do you want to do the offline stuff?" << endl;
+	cin >> answer;
+
+	if (answer == "y") {
+		cout << "read y " << endl;
+		handleOfflineStuff();
+	}
+	else if (answer == "n") {
+		cout << "read n" << endl;
+	}
+
+
+	cin >> answer;
+	//switch 
     GraphDef graph_def = CreateGraphDef();
 
 	//ops::Variable()
--- Tensorflow_Base/matmul.h
+++ Tensorflow_Base/matmul.h
@@ -3,4 +3,9 @@
 #define COMPILER_MSVC
 #define NOMINMAX
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int extractFeaturesFromVideo(std::string path);
 
