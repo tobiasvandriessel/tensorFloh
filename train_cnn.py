ef177a3709697065cb5bc90b498d07e35c51bf89
Tobias van Driessel <tobiasvandriessel@startmail.com>
2018-04-06 20:47:30 +0200

    basis of neutral network set

--- train_cnn.py
+++ train_cnn.py
@@ -90,3 +90,84 @@ def cnn_model_fn(features, labels, mode):
     dropout2 = tf.layers.dropout(inputs=dense2, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
     logits = tf.layers.dense(inputs=dropout2, units=5)
+
+
+    
+
+    predictions= {
+        "classes":  tf.argmax(input=logits, axis=1),
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")        
+    }
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=5)
+    loss = tf.losses.softmax_cross_entropy(
+        onehot_labels=onehot_labels, logits=logits
+    )
+
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01) #This changes over the iterations
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step()
+        )
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels, predictions=predictions["classes"]
+        )
+    }
+
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops
+    )
+
+def main(unused_argv):
+    # TODO
+    # Load training and eval data
+    mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+    train_data = mnist.train.images # Returns np.array
+    train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+    eval_data = mnist.test.images # Returns np.array
+    eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+    # Create the estimator
+    classifier = tf.estimator.Estimator(
+        model_fn=cnn_model_fn, model_dir="/tmp/convnet_model"
+    )
+
+    tensors_to_log = {"probabilities": "softmax_tensor"}
+    logging_hook = tf.train.LoggingTensorHook(
+        tensors=tensors_to_log, every_n_iter=50
+    )
+
+
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x", train_data},
+        y=train_labels,
+        batch_size=100,
+        num_epochs=None,
+        shuffle=True
+    )
+
+    classifier.train(
+        input_fn=train_input_fn,
+        steps=20000,
+        hooks=[logging_hook]
+    )
+
+    # Evaluate the model and print results
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x", eval_data},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False
+    )
+
+    eval_results = classifier.evaluate(input_fn=eval_input_fn)
+    print(eval_results)
