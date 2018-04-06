48e643cf1d23d36f4a5102f7591f443aa753eee7
Tobias van Driessel <tobiasvandriessel@startmail.com>
2018-04-06 14:40:12 +0200

    more

--- train_cnn.py
+++ train_cnn.py
@@ -23,10 +23,18 @@ def cnn_model_fn(features, labels, mode):
     padding="same",
     activation=tf.nn.relu)
 
-    norm1 = tf.nn.lrn(input_layer, 5, 2, 0.0001, 0.75)
+    norm1 = tf.nn.lrn(conv1, 5, 2, 0.0001, 0.75)
 
     pool1 = tf.layers.MaxPooling2D(inputs=norm1, pool_size=[2,2], strides=2)
 
-    
 
+    conv2 = tf.layers.conv2d(
+    inputs=pool1,
+    filters=256,
+    kernel_size=[5, 5],
+    padding="same",
+    activation=tf.nn.relu)
 
+    norm2 = tf.nn.lrn(conv2, 5, 2, 0.0001, 0.75)
+
+    pool2 = tf.layers.MaxPooling2D(inputs=norm2, pool_size=[2,2], strides=2)
